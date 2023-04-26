1dfd3a285b714b54c25e4be6769d3831334ca6f6
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-04-26 20:49:26 +0300

    Update __init__.py

--- roblox-aio/__init__.py
+++ roblox-aio/__init__.py
@@ -1,9 +1,9 @@
 """
 
-ro.py  
-A modern, asynchronous wrapper for the Roblox web API.
+roblox-aio.py
+A Python package that uses aiohttp to request Roblox data.
 
-Copyright 2020-present jmkdev  
+Copyright 2023-present RainzDev
 License: MIT, see LICENSE
 
 """
