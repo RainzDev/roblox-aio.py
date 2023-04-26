7b852a119072e4bfad8987f218f0325a241ef9c2
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-04-26 19:19:35 +0300

    Update user.py

--- roblox-aio/user.py
+++ roblox-aio/user.py
@@ -1,4 +1,20 @@
 import aiohttp
 import endpoints
+from .auth import authentication, get_csrf_token
 
-print("test")
+class User:
+    def __init__(self, cookie):
+        self.cookie = cookie
+        
+        
+    async def change_display_name(self, name: str):
+        data = {"newDisplayName": name}
+        _id = await authentication()
+        headers = {}
+        async with aiohttp.ClientSession() as session:
+            async with session.patch(f"{endpoints.users}/users/{_id}/display-names") as response:
+                r = await response.json()
+                if "errors" in r["data"]:
+                    pass
+                else:
+                    return True
