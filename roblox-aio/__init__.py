7722be89edf8705bcee9f7a5152690d13ba4d429
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-04-26 20:47:59 +0300

    Create __init__.py

---
+++ roblox-aio/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+ro.py  
+A modern, asynchronous wrapper for the Roblox web API.
+
+Copyright 2020-present jmkdev  
+License: MIT, see LICENSE
+
+"""
+
+__title__ = "roblox-aio.py"
+__author__ = "RainzDev"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present RainzDev"
+__version__ = "1.0.0"
