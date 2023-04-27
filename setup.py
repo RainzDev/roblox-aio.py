d09093e42a0da953f9def49b5071e8dba8a956cd
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-04-27 17:29:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,12 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup_info = {
-    "name": "roblox-aio",
-    "version": "1.0.0",
-    "author": "RainzDev",
-    "description": "roblox-aio.py is a Roblox API wrapper that uses aiohttp to grab the data.",
-    "long_description": long_description,
-    "long_description_content_type": "text/markdown",
-    "url": "https://github.com/RainzDev/roblox-aio.py/",
-    "packages": setuptools.find_packages(),
-    "classifiers": [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Framework :: AsyncIO",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
-        "Topic :: Software Development :: Libraries"
-    ],
-    "project_urls": {
-        "Issue Tracker": "https://github.com/RainzDev/roblox-aio.py/issues",
-        "GitHub": "https://github.com/RainzDev/roblox-aio.py/"
-    },
-    "python_requires": '>=3.9',
-    "install_requires": [
-        "aiohttp>=3.8.4",
-    ]
-}
-
-
-setuptools.setup(**setup_info)
+setup(
+    name='roblox-aio.py',
+    version='1.0.0',
+    description='A Roblox API wrapper used for getting data from Roblox API.',
+    author='RainzDev',
+    author_email='jessrblx16@gmail.com',
+    license='MIT',
+    packages=['roblox-aio.py'],
+    install_requires=['aiohttp'],
+)
