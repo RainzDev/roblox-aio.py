e99af3f1600a7a9a589ad17ea017b44683ef0773
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-04-27 18:40:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author='RainzDev',
     author_email='jessrblx16@gmail.com',
     license='MIT',
-    packages=['roblox-aio'],
+    packages=['roblox_aio'],
     install_requires=['numpy', 'aiohttp'],
 )
