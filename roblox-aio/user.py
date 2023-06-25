238807b1985d021ce7b318145b2397e26d4a229a
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-06-25 19:29:02 +0300

    Added raised errors

--- roblox-aio/user.py
+++ roblox-aio/user.py
@@ -1,6 +1,11 @@
 import aiohttp
 from .auth import authentication, get_csrf_token
 
+class InvalidDisplay(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
 class User:
     def __init__(self, cookie):
         self.cookie = cookie
@@ -17,7 +22,14 @@ class User:
         async with aiohttp.ClientSession() as session:
             async with session.patch(f"https://users.roblox.com/v1/users/{_id}/display-names") as response:
                 r = await response.json()
-                if "errors" in r["data"]:
-                    pass
-                else:
-                    return True
+				if "errors" in r["data"]:
+                	if r["data"]["errors"][0]["code"] == 4:
+                    	raise InvalidDisplay(f"String '{name}' cannot be displayed as it has been moderated")
+					elif r["data"]["errors"][0]["code"] == 3:
+                		raise InvalidDisplay(f"String '{name}' contains invalid characters")
+					elif r["data"]["errors"][0]["code"] == 2:
+                		raise InvalidDisplay(f"String size '{name}' is too long")
+					elif r["data"]["errors"][0]["code"] == 1:
+                		raise InvalidDisplay(f"String size '{name}' is too short")
+				else:
+					return r
