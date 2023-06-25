3f6cac52504427d49cab785997c6e17140801c19
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-06-25 19:30:07 +0300

    Fixed indents

--- roblox-aio/user.py
+++ roblox-aio/user.py
@@ -22,14 +22,14 @@ class User:
         async with aiohttp.ClientSession() as session:
             async with session.patch(f"https://users.roblox.com/v1/users/{_id}/display-names") as response:
                 r = await response.json()
-				if "errors" in r["data"]:
+		if "errors" in r["data"]:
                 	if r["data"]["errors"][0]["code"] == 4:
-                    	raise InvalidDisplay(f"String '{name}' cannot be displayed as it has been moderated")
-					elif r["data"]["errors"][0]["code"] == 3:
+                    		raise InvalidDisplay(f"String '{name}' cannot be displayed as it has been moderated")
+			elif r["data"]["errors"][0]["code"] == 3:
                 		raise InvalidDisplay(f"String '{name}' contains invalid characters")
-					elif r["data"]["errors"][0]["code"] == 2:
+			elif r["data"]["errors"][0]["code"] == 2:
                 		raise InvalidDisplay(f"String size '{name}' is too long")
-					elif r["data"]["errors"][0]["code"] == 1:
+			elif r["data"]["errors"][0]["code"] == 1:
                 		raise InvalidDisplay(f"String size '{name}' is too short")
-				else:
-					return r
+		else:
+			return r
