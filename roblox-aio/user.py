4c83595b721483d857fafb60e633082a4b48f98e
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-07-04 19:56:35 +0300

    Update user.py

--- roblox-aio/user.py
+++ roblox-aio/user.py
@@ -17,17 +17,17 @@ class User:
 		'.ROBLOSECURITY': self.cookie 
 		}
 		headers = {"x-csrf-token": await get_csrf_token(cookie=self.cookie)}
-        	async with aiohttp.ClientSession() as session:
-            	async with session.patch(f"https://users.roblox.com/v1/users/{_id}/display-names", json=data}) as response:
-                	r = await response.json()
-			if "errors" in r["data"]:
-            	if r["data"]["errors"][0]["code"] == 4:
-            		raise InvalidDisplay(f"String '{name}' cannot be displayed as it has been moderated")
-				elif r["data"]["errors"][0]["code"] == 3:
-                	raise InvalidDisplay(f"String '{name}' contains invalid characters")
-				elif r["data"]["errors"][0]["code"] == 2:
-                	raise InvalidDisplay(f"String size '{name}' is too long")
-				elif r["data"]["errors"][0]["code"] == 1:
-                	raise InvalidDisplay(f"String size '{name}' is too short")
-			else:
-				return r
+		async with aiohttp.ClientSession() as session:
+			async with session.patch(f"https://users.roblox.com/v1/users/{_id}/display-names", json=data}) as response:
+				r = await response.json()
+				if "errors" in r["data"]:
+					if r["data"]["errors"][0]["code"] == 4:
+						raise InvalidDisplay(f"String '{name}' cannot be displayed as it has been moderated")
+					elif r["data"]["errors"][0]["code"] == 3:
+						raise InvalidDisplay(f"String '{name}' contains invalid characters")
+					elif r["data"]["errors"][0]["code"] == 2:
+						raise InvalidDisplay(f"String size '{name}' is too long")
+					elif r["data"]["errors"][0]["code"] == 1:
+						raise InvalidDisplay(f"String size '{name}' is too short")
+				else:
+					return r
