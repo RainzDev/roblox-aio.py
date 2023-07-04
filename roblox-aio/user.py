fc0b81bc26e64a90c7d51ce61e48be907f647409
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-07-04 20:16:22 +0300

    Updated the descriptions

--- roblox-aio/user.py
+++ roblox-aio/user.py
@@ -3,11 +3,19 @@ from .auth import authentication, get_csrf_token
 from .errors import InvalidDisplay, CookieError
 
 class User:
+	"""Represents the user
+ 	Attributes:
+  		id: Grabs the current user ID
+		name: Grabs the current user name
+  		display_name: Grabs the current display name"""
     def __init__(self, cookie: str=None):
         self.cookie = cookie
         
         
     async def change_display_name(self, name: str):
+		"""Changes the account display name
+		
+  		"""
 		if not self.cookie:
 			raise CookieError
         data = {"newDisplayName": name}
