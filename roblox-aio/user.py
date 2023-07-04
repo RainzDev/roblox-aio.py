54b92c7484fa13e47187533a4ff8f2509094db8c
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-07-04 19:52:59 +0300

    Update user.py

--- roblox-aio/user.py
+++ roblox-aio/user.py
@@ -8,26 +8,26 @@ class User:
         
         
     async def change_display_name(self, name: str):
-	if not self.cookie:
-		raise CookieError
+		if not self.cookie:
+			raise CookieError
         data = {"newDisplayName": name}
         auth = authentication(cookie=self.cookie)
         _id = await auth.get_auth()['id']
         cookies = {
 		'.ROBLOSECURITY': self.cookie 
-	}
-	headers = {"x-csrf-token": await get_csrf_token(cookie=self.cookie)}
-        async with aiohttp.ClientSession() as session:
-            async with session.patch(f"https://users.roblox.com/v1/users/{_id}/display-names", json=data}) as response:
-                r = await response.json()
-		if "errors" in r["data"]:
-                	if r["data"]["errors"][0]["code"] == 4:
-                    		raise InvalidDisplay(f"String '{name}' cannot be displayed as it has been moderated")
-			elif r["data"]["errors"][0]["code"] == 3:
-                		raise InvalidDisplay(f"String '{name}' contains invalid characters")
-			elif r["data"]["errors"][0]["code"] == 2:
-                		raise InvalidDisplay(f"String size '{name}' is too long")
-			elif r["data"]["errors"][0]["code"] == 1:
-                		raise InvalidDisplay(f"String size '{name}' is too short")
-		else:
-			return r
+		}
+		headers = {"x-csrf-token": await get_csrf_token(cookie=self.cookie)}
+        	async with aiohttp.ClientSession() as session:
+            	async with session.patch(f"https://users.roblox.com/v1/users/{_id}/display-names", json=data}) as response:
+                	r = await response.json()
+			if "errors" in r["data"]:
+            	if r["data"]["errors"][0]["code"] == 4:
+            		raise InvalidDisplay(f"String '{name}' cannot be displayed as it has been moderated")
+				elif r["data"]["errors"][0]["code"] == 3:
+                	raise InvalidDisplay(f"String '{name}' contains invalid characters")
+				elif r["data"]["errors"][0]["code"] == 2:
+                	raise InvalidDisplay(f"String size '{name}' is too long")
+				elif r["data"]["errors"][0]["code"] == 1:
+                	raise InvalidDisplay(f"String size '{name}' is too short")
+			else:
+				return r
