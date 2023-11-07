4c22b34b181a801bc0c813b5aa4babbf54179a0b
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-11-07 10:02:12 -0800

    Moved csrf token function inside of the class

--- roblox-aio/auth.py
+++ roblox-aio/auth.py
@@ -1,23 +1,23 @@
 import aiohttp
 
-async def get_csrf_token(self, cookie: str):
-	"""Used for getting the x-csrf-token by using the logout endpoint."""
-	cookies = {
-		'.ROBLOSECURITY': cookie
-	}
-	async with aiohttp.ClientSession() as session:
-		async with session.post(f"https://auth.roblox.com/v2/logout", cookies=cookies) as r:
-			return r.headers['x-csrf-token']
-
 class authentication:
-    def __init__(self, cookie):
-        self.cookie = cookie
-        
-    async def get_auth(self):
-        cookies = {
+	def __init__(self, cookie):
+		self.cookie = cookie
+
+	async def get_csrf_token(self):
+		"""Used for getting the x-csrf-token by using the logout endpoint."""
+		cookies = {
+			'.ROBLOSECURITY': self.cookie
+		}
+		async with aiohttp.ClientSession() as session:
+			async with session.post(f"https://auth.roblox.com/v2/logout", cookies=cookies) as r:
+				return r.headers['x-csrf-token']
+		
+	async def get_auth(self):
+		cookies = {
 		'.ROBLOSECURITY': self.cookie 
-	}
-	headers = {"x-csrf-token": await get_csrf_token(cookie=self.cookie)}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://users.roblox.com/v1/users/authenticated", cookies=cookies, headers=headers) as response:
-                return await response.json()
+		}
+		headers = {"x-csrf-token": await self.get_csrf_token()}
+		async with aiohttp.ClientSession() as session:
+			async with session.get(f"https://users.roblox.com/v1/users/authenticated", cookies=cookies, headers=headers) as response:
+				return await response.json()
